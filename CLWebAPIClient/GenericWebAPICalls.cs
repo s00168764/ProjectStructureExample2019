f1ed92401c2720fac34220a6f2726865bb0be572
William McLaughlin <s00168764@mail.itsligo.ie>
2019-03-29 11:47:52 +0000

    Comments added

--- CLWebAPIClient/GenericWebAPICalls.cs
+++ CLWebAPIClient/GenericWebAPICalls.cs
@@ -14,7 +14,7 @@ using System.Net;
 
 namespace WebAPIAuthenticationClient
 {
-    public enum AUTHSTATUS { NONE,OK,INVALID,FAILED }
+    public enum AUTHSTATUS { NONE, OK, INVALID, FAILED }
 
 
     public static class ClientAuthentication
@@ -28,16 +28,21 @@ namespace WebAPIAuthenticationClient
         static public List<T> getList<T>(string endpoint)
         {
             using (var client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthToken);
-                var response = client.GetAsync(baseWebAddress + endpoint ).Result;
-                    var resultContent = response.Content.ReadAsAsync<List<T>>(
-                        new[] { new JsonMediaTypeFormatter() }).Result;
-                    return resultContent;
-                }
+            {
+                // clear the current reuest header
+                client.DefaultRequestHeaders.Accept.Clear();
+                // add new header
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                // authorize the new header with a token
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthToken);
+                // get clients at address
+                var response = client.GetAsync(baseWebAddress + endpoint).Result;
+                var resultContent = response.Content.ReadAsAsync<List<T>>(
+                    new[] { new JsonMediaTypeFormatter() }).Result;
+                // return an asyncronous list of responses
+                return resultContent;
             }
+        }
 
         static public dynamic getExtGame(int gameID)
         {
@@ -53,8 +58,8 @@ namespace WebAPIAuthenticationClient
                 var jsummary = resultContent.Children()["summary"].Values<string>().FirstOrDefault();
                 // url is nested in cover object
                 var jcover = resultContent.Children()["cover"]["url"].Values<string>().FirstOrDefault();
-                var eobj = 
-                                        new 
+                var eobj =
+                                        new
                                         {
                                             Name = jname,
                                             Summary = jsummary,
@@ -76,15 +81,15 @@ namespace WebAPIAuthenticationClient
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthToken);
                     var response = client.GetAsync(baseWebAddress + EndPoint).Result;
-                    if(response.StatusCode == HttpStatusCode.OK && response.ReasonPhrase.Contains("no accounts to manage"))
+                    if (response.StatusCode == HttpStatusCode.OK && response.ReasonPhrase.Contains("no accounts to manage"))
                     {
-                        throw new Exception("Information from getItem call ",new Exception(response.ReasonPhrase));
+                        throw new Exception("Information from getItem call ", new Exception(response.ReasonPhrase));
                     }
                     var resultContent = response.Content.ReadAsAsync<T>(
                         new[] { new JsonMediaTypeFormatter() }).Result;
                     return resultContent;
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     throw (ex);
                 }
@@ -101,8 +106,8 @@ namespace WebAPIAuthenticationClient
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthToken);
-                var response = client.PostAsJsonAsync(baseWebAddress + Endpoint,g).Result;
-                if(response.IsSuccessStatusCode)
+                var response = client.PostAsJsonAsync(baseWebAddress + Endpoint, g).Result;
+                if (response.IsSuccessStatusCode)
                 {
                     Console.WriteLine("Response Object is " + response.Content.ReadAsAsync<T>(
                         new[] { new JsonMediaTypeFormatter() }).Result.ToString());
@@ -112,7 +117,7 @@ namespace WebAPIAuthenticationClient
             }
 
         }
-    static public bool login(string username, string password)
+        static public bool login(string username, string password)
         {
             using (var client = new HttpClient())
             {
@@ -140,7 +145,7 @@ namespace WebAPIAuthenticationClient
                     }
                     else
                     {
-                        AuthToken = "Invalid Login" ;
+                        AuthToken = "Invalid Login";
                         AuthStatus = AUTHSTATUS.INVALID;
                         Console.WriteLine("Invalid credentials");
                         return false;
@@ -157,7 +162,7 @@ namespace WebAPIAuthenticationClient
             }
         }
 
-        
 
-}
+
+    }
 }
--- ProjectStructureExampleConsoleApp/Program.cs
+++ ProjectStructureExampleConsoleApp/Program.cs
@@ -13,15 +13,21 @@ namespace ProjectStructureExampleConsoleApp
     {
         static void Main(string[] args)
         {
+            // change depending on machine
             ClientAuthentication.baseWebAddress = "http://localhost:51132/";
+            // checks the authentication login username and password
             if (ClientAuthentication.login("bowles.lionie@itsligo.ie", "LBowles$1"))
             {
+                // prints token and login status
                 Console.WriteLine("Successful login Token acquired {0} user status is {1}", ClientAuthentication.AuthToken,ClientAuthentication.AuthStatus.ToString());
                 try
                 {
+                    // tries to get the model view
                     AccountUserViewModel acuvm = ClientAuthentication.getItem<AccountUserViewModel>("api/AccountManager/getAccounts");
+                    // if successful
                     if (acuvm != null)
                     {
+                        // print the account names
                         Console.WriteLine("Got {0} accounts for current logged in user {1}", acuvm.accounts.Count(), acuvm.AccountManagerName);
                         // Get account list using current UserID
                         List<Account> accounts = ClientAuthentication.getList<Account>("api/AccountManager/getAccountsForCurrentManager/" + acuvm.AccountManagerID);
